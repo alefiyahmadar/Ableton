a58f2d720f9489eda7a75231edf76eb4ab974094
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2024-11-24 20:51:34 +0530

    initial-commit

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,3 +1,4 @@
+
 export const AboutPg =()=>{
 
     return(<div className="container">
@@ -8,7 +9,14 @@ export const AboutPg =()=>{
                 <li>Apprenticeships</li>
             </ul>
         </div>
-        <div className="imgDiv"></div>
+        <div className="imgDiv">
+            <p>Ableton</p>
+        </div>
+        <div className="info">
+
+        <h3>We make Live, Push, Note and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h3>
+        <h4>Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</h4>
+        </div>
     </div>)
 
 }

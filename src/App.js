1ef45b2b0980099297a60d015efcf19b5fac94eb
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2024-11-28 20:46:58 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Routes, Route, NavLink } from "react-router-dom";
 import { AboutPg } from "./pages/AboutPage";
 import { JobPg } from "./pages/Jobs";
 import { ApprenticePg } from "./pages/Apprentice";
+import { HomePage } from "./pages/HomePg";
 
 
 export default function App() {
   return (
     <div className="App">
       <nav className="nav">
+        <NavLink to={"/home"}><img  width={50} height={50} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAU1JREFUSEu91M0rBVEcxvHPJZIFWbOwlITIxsLLQhb8JUrYWFDKQpRSxN9BKQspNlJeSmwtLOyVjVJe5mhuXRNzzR3XqdnMOZ3v7zm/5/kVVHkVqny/vIA6XOMNvXhNFpwXMIeN+NJZbP4loAX3EaApvvQJ7XgsheRRsIOpRMXbmP4LQCduUJsAhB504K74v1IFJxj+wYGHGM8DmMR+GXtP4CCcqUTBLbrKAMLz9VQCqMF5pKC/DOAUQyEflSjIFP5/BRxjJKW84JzRkv1GrCI0tA0P0bjYw0L0vXznovdfaC8qro978dnIxAo9GizOpdInygJYihy4nFLQDLaSLsoCCBP0u+qLzLNYxZccZAE8oyFFQRh8zXkUXKEvBXCJgTyA4JSVFMA81vMAgosu0J3FReVycISxRA7W4hy0xjnYxeJPOfhFDLIfqfqo+ADdZzkZL6kfrAAAAABJRU5ErkJggg=="/></NavLink>
       
-        <img width={50} height={50} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAU1JREFUSEu91M0rBVEcxvHPJZIFWbOwlITIxsLLQhb8JUrYWFDKQpRSxN9BKQspNlJeSmwtLOyVjVJe5mhuXRNzzR3XqdnMOZ3v7zm/5/kVVHkVqny/vIA6XOMNvXhNFpwXMIeN+NJZbP4loAX3EaApvvQJ7XgsheRRsIOpRMXbmP4LQCduUJsAhB504K74v1IFJxj+wYGHGM8DmMR+GXtP4CCcqUTBLbrKAMLz9VQCqMF5pKC/DOAUQyEflSjIFP5/BRxjJKW84JzRkv1GrCI0tA0P0bjYw0L0vXznovdfaC8qro978dnIxAo9GizOpdInygJYihy4nFLQDLaSLsoCCBP0u+qLzLNYxZccZAE8oyFFQRh8zXkUXKEvBXCJgTyA4JSVFMA81vMAgosu0J3FReVycISxRA7W4hy0xjnYxeJPOfhFDLIfqfqo+ADdZzkZL6kfrAAAAABJRU5ErkJggg=="/>
+        
         <div className="nav-items">
         <p>Live</p>
         <p>Push</p>
@@ -35,6 +37,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<AboutPg />}></Route>
+        <Route path="/home" element={<HomePage/>}></Route>
         <Route path="/jobs" element={<JobPg/>}></Route>
         <Route path="/Apprentice" element={<ApprenticePg/>}></Route>
       </Routes>
---
+++ src/pages/HomePg.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom"
+
+export const HomePage =()=>{
+
+return(<div className="containerHome">
+    <div className="containerHomeImg">
+        <p>Live is back in stock</p>
+        <NavLink className={"homeLink"}>Shop Now</NavLink>
+
+    </div>
+    <div className="HomeGetMove">
+        <h3>Get  started with Move</h3>
+
+
+    </div>
+
+    
+</div>)
+
+} 

9437a2feff0f4663e5edf70d57c7f09c435769e1
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2025-01-10 11:24:52 +0530

    initial-commit

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -39,12 +39,13 @@ export const AboutPg =()=>{
     </div>
    
 </div>
-<div className="containerVid">
 <div className="container-for-text">
         <h3 >Making music isn’t easy. It takes time, effort, and learning. But when you’re in the flow, it’s incredibly rewarding.</h3>
         
         <h4> We feel the same way about making Ableton products. The driving force behind Ableton is our passion for what we make, and the people we make it for.</h4>
     </div>
+<div className="containerVid">
+
 <div className="vid">
     
       

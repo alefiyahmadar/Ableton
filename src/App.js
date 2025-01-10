9f88ac088d5362c594d6bd4120904f37259c3292
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2025-01-10 19:15:34 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ export default function App() {
   return (
     <div className="App">
       <nav className="nav">
-        <NavLink to={"/home"}><img style={{display:"block"}} width={50} height={50} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAU1JREFUSEu91M0rBVEcxvHPJZIFWbOwlITIxsLLQhb8JUrYWFDKQpRSxN9BKQspNlJeSmwtLOyVjVJe5mhuXRNzzR3XqdnMOZ3v7zm/5/kVVHkVqny/vIA6XOMNvXhNFpwXMIeN+NJZbP4loAX3EaApvvQJ7XgsheRRsIOpRMXbmP4LQCduUJsAhB504K74v1IFJxj+wYGHGM8DmMR+GXtP4CCcqUTBLbrKAMLz9VQCqMF5pKC/DOAUQyEflSjIFP5/BRxjJKW84JzRkv1GrCI0tA0P0bjYw0L0vXznovdfaC8qro978dnIxAo9GizOpdInygJYihy4nFLQDLaSLsoCCBP0u+qLzLNYxZccZAE8oyFFQRh8zXkUXKEvBXCJgTyA4JSVFMA81vMAgosu0J3FReVycISxRA7W4hy0xjnYxeJPOfhFDLIfqfqo+ADdZzkZL6kfrAAAAABJRU5ErkJggg=="/></NavLink>
+        <NavLink to={"/home"}><img style={{display:"block"}} width="50" height="50" src="https://img.icons8.com/material/24/geometric-figures.png" alt="geometric-figures"/></NavLink>
         
-      
+      <p className="menu">Menu<img style={{display:"block" }} width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/sort-down.png" alt="sort-down"/></p>
         
         <div className="nav-items">
         <p>Live</p>
